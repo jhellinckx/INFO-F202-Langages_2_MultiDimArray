983c3f82ed4cd02136f93288bc115824fea06e55
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2014-10-13 18:44:40 +0200

    [add] class ArrayLengths - still have to refactor with it

--- Vec.cpp
+++ Vec.cpp
@@ -12,83 +12,114 @@
 	RESIZE
 */
 
+namespace Constants{
+	static const int DEFAULT_DIM = 1;
+	static const int DEFAULT_DIM_LENGTH = 1;
+}
+
+class ArrayLengths{
+	std::size_t _dim;
+	std::size_t* _lengths;
+
+public:
+	ArrayLengths() : _dim(Constants.DEFAULT_DIM), _lengths(new std::size_t[Constants.DEFAULT_DIM]){
+		for(std::size_t i=0;i<_dim;++i)
+			_lengths[i] = Constants.DEFAULT_DIM_LENGTH;
+	}
+	ArrayLengths(std::size_t dimension, std::size_t* lengths = NULL) : _dim(dimension), _lengths(new std::size_t[dimension]){
+		if(dimension<1) throw std::logic_error("Dimension cannot be lesser than 1");
+		if(lengths==NULL){
+			for(std::size_t i=0;i<_dim;++i) _lengths[i]=Constants.DEFAULT_DIM_LENGTH;
+		}
+		else
+			for(std::size_t i=0;i<_dim;++i) _lengths[i]=lengths[i];
+	}
+	ArrayLengths(const ArrayLengths& other) : _dim(other._dim), _lengths(new std::size_t[other._dim]) {
+		for(std::size_t i=0;i<_dim;++i) _lengths[i]=other._lengths[i];
+	}
+
+	ArrayLengths& operator=(const ArrayLengths& other){
+		if(this!=&other){
+			_dim = other_dim; 
+			delete[] _lengths; _lengths = new std::size_t[other._dim];
+			for(std::size_t i=0;i<_dim;++i) _lengths[i]=other._lengths[i];
+		}
+		return *this;
+	}
+ 	
+ 	std::size_t getDimension() const { return _dim; }
+ 	std::size_t get(std::size_t index){
+ 		if(index>_dim) throw std::out_of_range("Index out of range");
+ 		return _lengths[index];
+	}
+	void set(std::size_t index, std::size_t length){
+		 if(index>_dim) throw std::out_of_range("Index out of range");
+		 _lengths[index] = length;
+	}
+
+	~ArrayLengths(){ delete[] _lengths; }
+};
+
 
 /* Classe abstraite représentant un objet ayant plusieurs dimensions */
-class DimensionalObject{
-	std::size_t _dim;
-	std::size_t* _lengths;	//Longueur de chaque dimension
+template<typename Elem, std::size_t DIM>
+class DimensionalContainer{
+	Elem* _val;
+	ArrayLengths _lengths;
 
 protected:
-	DimensionalObject(std::size_t dim, std::size_t lengths[]) : _dim(dim), _lengths(new std::size_t[dim]){
-		if(dim < 1)
-			throw std::out_of_range("Dimension cannot be lesser than 1");
-		for(std::size_t i=0;i<dim;++i)
-			_lengths[i] = lengths[i];
-	}
+	DimensionalContainer() : _val(NULL), _lengths(ArrayLengths(DIM)) {}
+	DimensionalContainer(ArrayLengths& lengths) : _val(NULL), _dim(DIM), _lengths(lengths){}
 	std::size_t* getLengthsArray() const { return _lengths; }
 	std::size_t* getSubLengthsArray() const { 				//Longueurs des dimensions de la sous-dimension
-		if(_dim==0)
-			throw std::out_of_range("Index out of range");
+		if(_dim==1) throw std::out_of_range("Array of dimension 1 has no subarray");
 		return _lengths+1; 
 	}
 	void setLengths(std::size_t* copyLengths, std::size_t limit){
-		if(limit>_dim)
-			throw std::out_of_range("Index out of range");
-		for(std::size_t i=0;i<limit;++i){
-			_lengths[i] = copyLengths[i];
-		}
+		if(limit>_dim) throw std::out_of_range("Index out of range");
+		for(std::size_t i=0;i<limit;++i) _lengths[i] = copyLengths[i];
 	}
-	bool hasSameLengths(const DimensionalObject& other) {
-		if(_dim!=other._dim)
-			return false; 
-		for(std::size_t i =0;i<_dim;++i){
-			if(_lengths[i]!=other._lengths[i])
-				return false;
-		} 
-		return true;
-	}
+	
 	std::size_t computeSize(std::size_t dim, std::size_t* lengths) const{
-		if(dim>_dim)
-			throw std::out_of_range("Index out of range");
+		if(dim>_dim) throw std::out_of_range("Index out of range");
 		std::size_t size = 1;
 		for(std::size_t i=0;i<_dim;++i){
-			if(i<_dim-dim)
-				size *= _lengths[i];
+			if(i<_dim-dim) size *= _lengths[i];
 		}
-		for(std::size_t i=0;i<dim;++i){
-			size *= lengths[i];
-		}
+		for(std::size_t i=0;i<dim;++i) size *= lengths[i];
 		return size;
 	}
-	virtual void resize(DimensionalObject& from, DimensionalObject& to){}
-public: 
+
+public:
 	std::size_t getDimension() const { return _dim; } 
 	std::size_t getLength(std::size_t i=0) const { 
-		if(i>=_dim)
-			throw std::out_of_range("Index out of range");
+		if(i>=_dim) throw std::out_of_range("Index out of range");
 		return _lengths[i]; 
 	}
 	std::size_t getSubLength() const { 						//Longueur de la sous-dimension
-		if(_dim==1)
-			throw std::out_of_range("Index out of range");
+		if(_dim==1) throw std::out_of_range("Index out of range");
 		return _lengths[1]; 
 	} 	
 	std::size_t getSize(std::size_t dimI = 0) const { 		//Nombre d'éléments compris dans cette dimension
-		if(dimI>=_dim)
-			throw std::out_of_range("Index out of range");
+		if(dimI>=_dim) throw std::out_of_range("Index out of range");
 		std::size_t l = 1; 
-		for(;dimI<_dim;++dimI) 
-			l*=_lengths[dimI]; 
+		for(;dimI<_dim;++dimI) l*=_lengths[dimI]; 
 		return l;
 	}
 	std::size_t getSubSize() const { 
-		if(_dim==1)
-			throw std::out_of_range("Index out of range");
+		if(_dim==1) throw std::out_of_range("Index out of range");
 		return getSize(1); 
 	}
+	bool hasSameLengths(const DimensionalContainer& other) {
+		if(_dim!=other._dim) return false; 
+		for(std::size_t i =0;i<_dim;++i){
+			if(_lengths[i]!=other._lengths[i]) return false;
+		} 
+		return true;
+	}
 		
-	DimensionalObject(const DimensionalObject& other) : DimensionalObject(other._dim, other._lengths) {}
-	DimensionalObject& operator=(const DimensionalObject& other){
+	DimensionalContainer(const DimensionalContainer& other) : DimensionalContainer(other._dim, other._lengths){}
+	DimensionalContainer& operator=(const DimensionalContainer& other){
 		if(this != &other){
 			_dim = other._dim;
 			delete[] _lengths; _lengths = new std::size_t[_dim];
@@ -96,7 +127,7 @@ public:
 		}
 		return *this;
 	}
-	virtual ~DimensionalObject(){ delete[] _lengths; }
+	virtual ~DimensionalContainer(){ delete[] _lengths; }
 };
 
 //------------------------------------------------------------
@@ -110,15 +141,12 @@ class MultiDimensionalArray<Elem, 1>;
 //------------------------------------------------------------
 
 template<typename Elem, std::size_t DIM>
-class SubDimensionArray: public DimensionalObject{
+class SubDimensionArray: public DimensionalContainer{
 	Elem* const _val;
-	DimensionalObject* const _masterArray;
 
 public:
-	SubDimensionArray(DimensionalObject* const master, std::size_t lengths[], Elem* val): 
-		DimensionalObject(DIM, lengths), _val(val), _masterArray(master) {}
-	SubDimensionArray(const SubDimensionArray<Elem, DIM>& other): 
-		DimensionalObject(other), _val(other._val), _masterArray(other._masterArray) {}
+	SubDimensionArray(std::size_t lengths[], Elem* val): DimensionalContainer(DIM, lengths), _val(val){}
+	SubDimensionArray(const SubDimensionArray<Elem, DIM>& other): DimensionalContainer(other), _val(other._val){}
 	SubDimensionArray(SubDimensionArray<Elem, DIM>&&);
 	SubDimensionArray<Elem, DIM>& operator=(const SubDimensionArray<Elem, DIM>& other){
 		if(this != &other){
@@ -128,8 +156,7 @@ public:
 				}
 				return *this;
 			}
-			_masterArray->resize(other);
-			
+			throw std::logic_error("Cannot assign subarrays of different lengths");
 		}
 		return *this;
 	}
@@ -139,12 +166,12 @@ public:
 	SubDimensionArray<Elem, DIM-1> operator[](std::ptrdiff_t i){
 		if(std::size_t(i)>=this->getLength())
 			throw std::out_of_range("Index for dimension out of range");
-		return SubDimensionArray<Elem, DIM-1>(_masterArray, this->getSubLengthsArray(), _val+i*this->getSubSize());
+		return SubDimensionArray<Elem, DIM-1>(this->getSubLengthsArray(), _val+i*this->getSubSize());
 	}
 	SubDimensionArray<Elem, DIM-1> operator[](std::ptrdiff_t i) const{
 		if(std::size_t(i)>=this->getLength())
 			throw std::out_of_range("Index for dimension out of range");
-		return SubDimensionArray<Elem, DIM-1>(_masterArray, this->getSubLengthsArray(), _val+i*this->getSubSize());
+		return SubDimensionArray<Elem, DIM-1>(this->getSubLengthsArray(), _val+i*this->getSubSize());
 	}
 	virtual ~SubDimensionArray(){}
 };
@@ -152,19 +179,16 @@ public:
 //------------------------------------------------------------
 
 template<typename Elem>
-class SubDimensionArray<Elem, 1>: public DimensionalObject{
+class SubDimensionArray<Elem, 1>: public DimensionalContainer{
 	Elem* const _val;
-	DimensionalObject* const _masterArray;
 
 public:
-	SubDimensionArray(DimensionalObject* const master, std::size_t lengths[], Elem* val): 
-		DimensionalObject(1, lengths), _val(val), _masterArray(master) {}
-	SubDimensionArray(const SubDimensionArray<Elem, 1>& other): 
-		DimensionalObject(other), _val(other._val), _masterArray(other._masterArray) {}
+	SubDimensionArray(std::size_t lengths[], Elem* val): DimensionalContainer(1, lengths), _val(val) {}
+	SubDimensionArray(const SubDimensionArray<Elem, 1>& other): DimensionalContainer(other), _val(other._val) {}
 	SubDimensionArray(SubDimensionArray<Elem, 1>&&); 
 	SubDimensionArray<Elem, 1>& operator=(const SubDimensionArray<Elem, 1>& other){
 		if(this != &other){
-			DimensionalObject::operator=(other);
+			DimensionalContainer::operator=(other);
 			_val = other._val;
 		}
 		return *this;
@@ -188,9 +212,8 @@ public:
 
 /* Classe représentant un tableau de dimension potentiellement infine */
 template<typename Elem, std::size_t DIM>
-class MultiDimensionalArray: public DimensionalObject {
+class MultiDimensionalArray: public DimensionalContainer {
 	Elem* _val;
-
 
 protected:
 	void copyAt(void* ptr,std::size_t limit){
@@ -201,8 +224,7 @@ protected:
 		}
 	}
 
-	template<typename T, std::size_t D>
-	void resize(SubDimensionArray<T, D>& from, SubDimensionArray<T,D>& to){
+	void* resize(DimensionalContainer& from, DimensionalContainer& to){
 		std::size_t newSize = this->computeSize(from.getDimension(), to.getLengthsArray());
 		Elem* tmp = new Elem[newSize];
 		copyAt(tmp,newSize);
@@ -215,15 +237,15 @@ public:
 	SubDimensionArray<Elem, DIM-1> operator[](std::ptrdiff_t i){
 		if(std::size_t(i)>=this->getLength())
 			throw std::out_of_range("Index for dimension out of range");
-		return SubDimensionArray<Elem, DIM-1>(this, this->getSubLengthsArray(), _val+i*this->getSubSize());
+		return SubDimensionArray<Elem, DIM-1>(this->getSubLengthsArray(), _val+i*this->getSubSize());
 	}
 	SubDimensionArray<Elem, DIM-1> operator[](std::ptrdiff_t i) const{
 		if(std::size_t(i)>=this->getLength())
 			throw std::out_of_range("Index for dimension out of range");
-		return SubDimensionArray<Elem, DIM-1>(const_cast<MultiDimensionalArray<Elem, DIM>*>(this), this->getSubLengthsArray(), _val+i*this->getSubSize());
+		return SubDimensionArray<Elem, DIM-1>(this->getSubLengthsArray(), _val+i*this->getSubSize());
 	}
 	explicit MultiDimensionalArray(std::size_t lengths[]):
-		DimensionalObject(DIM, lengths), _val(new Elem[this->getSize()]){}
+		DimensionalContainer(DIM, lengths), _val(new Elem[this->getSize()]){}
 	MultiDimensionalArray(std::size_t lengths[], const Elem& elem) : MultiDimensionalArray(lengths){
 		for(std::size_t i=0;i<this->getSize();++i)
 			_val[i] = elem;
